4df4c00350488da1211c7a14a93e4aa1e5b4033d
Roman Lubij <roman.lubij@gmail.com>
2026-02-24 23:10:33 +0100

    Add c++ shell

---
+++ cpp/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Central C++ Dev Shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          gcc
+          cmake
+          ninja
+          gdb
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+        ];
+        shellHook = ''
+          export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+        '';
+      };
+    };
+}
