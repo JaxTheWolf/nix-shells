4198e5933c5d63ebf1bf2b4430c814de09ee23a8
Roman Lubij <roman.lubij@gmail.com>
2026-02-27 21:32:50 +0100

    Run treefmt

--- cpp/flake.nix
+++ cpp/flake.nix
@@ -2,11 +2,13 @@
   description = "Central C++ Dev Shell";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    in
+    {
       devShells.${system}.default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
           gcc
